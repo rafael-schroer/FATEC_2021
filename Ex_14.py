84883dda082e1a2c72e5a0de446e0b85e34410cf
rafael-schroer <57038899+rafael-schroer@users.noreply.github.com>
2021-11-08 23:27:58 -0300

    Delete Ex_14.py

--- Ex_14.py
+++
@@ -1,6 +0,0 @@
-# Entrar via teclado com três valores distintos. Exibir o maior deles.
-num1 = int(input("Digite um número: "))
-num2 = int(input("Digite outro número: "))
-num3 = int(input("Digite mais um número: "))
-
-print("O maior número é: ", max(num1, num2, num3))
