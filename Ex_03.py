e284ec5770daa001f37fa3e9a70d9cb9494ffa08
rafael-schroer <57038899+rafael-schroer@users.noreply.github.com>
2021-11-08 23:25:11 -0300

    Delete Ex_03.py

--- Ex_03.py
+++
@@ -1,6 +0,0 @@
-# A partir dos valores da base e altura de um triângulo, calcular e exibir sua área.
-
-base = float(input("Digite a base do triângulo: "))
-altura = float(input("Digite a altura do triângulo: "))
-area= base * altura / 2
-print("A área do triângulo é: ", area)
