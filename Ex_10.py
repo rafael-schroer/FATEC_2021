3cc4b6d7c8153a0b0ab4470be78904717b36cecc
rafael-schroer <57038899+rafael-schroer@users.noreply.github.com>
2021-11-08 23:25:48 -0300

    Delete Ex_10.py

--- Ex_10.py
+++
@@ -1,7 +0,0 @@
-# Entrar via teclado, com dois valores distintos. Exibir o menor deles.
-num1 = int(input("Digite um número: "))
-num2 = int(input("Digite outro número: "))
-if num1 > num2:
-    print("O menor número é: ", num2)
-else:
-    print("O menor número é: ", num1)
