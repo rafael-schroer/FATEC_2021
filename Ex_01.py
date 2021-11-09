3cdb42d392d599427673a3a9ae5fea69eb0b9575
rafael-schroer <57038899+rafael-schroer@users.noreply.github.com>
2021-11-08 23:24:25 -0300

    Delete Ex_01.py

--- Ex_01.py
+++
@@ -1,8 +0,0 @@
-# Entrar via teclado com a base e a altura de um retângulo, calcular e exibir sua área.
-
-base = float(input("Digite a base do retângulo: "))
-altura = float(input("Digite a altura do retângulo: "))
-
-area = base * altura
-
-print("A área do retângulo é: ", area)
