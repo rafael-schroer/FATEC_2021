eb2d0aa2fe7c848394ea9303dac47d2a82082233
rafael-schroer <57038899+rafael-schroer@users.noreply.github.com>
2021-11-08 23:25:05 -0300

    Delete Ex_02.py

--- Ex_02.py
+++
@@ -1,6 +0,0 @@
-# Calcular e exibir a área de um quadrado, a partir do valor de sua aresta(lado) que será digitado.
-
-aresta = float(input("Digite o valor da aresta do quadrado: "))
-
-print("A área do quadrado é: ", aresta ** 2)
-
