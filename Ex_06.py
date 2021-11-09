7a7c77f3b004b06ba5783173011bb5030debb32e
rafael-schroer <57038899+rafael-schroer@users.noreply.github.com>
2021-11-08 23:25:28 -0300

    Delete Ex_06.py

--- Ex_06.py
+++
@@ -1,7 +0,0 @@
-# Entrar via teclado com o valor da cotação do dólar e uma certa quantidade de dólares. Calcular e exibir o valor correspondente em Reais (R$).
-cotacaodolar = float(input("Digite o valor da cotação do dólar: "))
-quantidadedolar = float(input("Digite a quantidade de dólares: "))
-
-reais = cotacaodolar * quantidadedolar
-
-print("O valor em reais é: R$ %.2f" % reais)
