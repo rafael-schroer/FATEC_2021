cabd9413c4e5a284e8386c5ba7346bf49ebae3c2
rafael-schroer <57038899+rafael-schroer@users.noreply.github.com>
2021-11-08 23:27:12 -0300

    Delete Ex_20.py

--- Ex_20.py
+++
@@ -1,9 +0,0 @@
-# Uma escola com cursos em regime semestral, realiza duas avaliações durante o semestre e calcula a média do aluno, da seguinte maneira: MEDIA = (P1 + 2P2) / 3 Fazer um programa para entrar via teclado com os valores das notas (P1 e P2) e calcular a média. Exibir a situação final do aluno (“Aprovado ou Reprovado”), sabendo que a média de aprovação é igual a cinco.
-
-nota1=float(input("Digite a primeira nota: "))
-nota2=float(input("Digite a segunda nota: "))
-media=(nota1+2*nota2)/3
-if media>=5:
-    print("Aprovado")
-else:
-    print("Reprovado")
